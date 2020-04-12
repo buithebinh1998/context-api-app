dbe88a4552daf3a3a40f87c096c98f3a3fe656fe
unknown <buithebinh@gmail.com>
2020-04-12 13:51:15 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import UserForm from './components/UserForm/UserForm'
 import UserList from './components/UserList/UserList'
-import {UserProvider} from './components/UserContext'
+import {UserContext} from './components/UserContext'
+
+
 function App() {
+  const [users, setUsers] = useState([
+    {id: '1', name: 'Binh', email: 'binh.buithe@pycogroup.com', active: false}
+  ]); 
+
   return (
-    <UserProvider>
+    <UserContext.Provider value={[users, setUsers]}>
       <div className="App">
         <UserForm/>
         <UserList/>
       </div>
-    </UserProvider>
+    </UserContext.Provider>
   );
 }
 
